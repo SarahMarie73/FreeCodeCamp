b1f22485c04c1278c4e86940709f149bc64c3d9e
Sarah Schlueter <sarahmarie73@gmail.com>
2024-01-14 22:43:22 -0600

    add monsters and build out fight functions

--- Build Role Playing Game/script.js
+++ Build Role Playing Game/script.js
@@ -33,6 +33,23 @@ const weapons = [
         power: 100
     }
 ];
+const monsters = [
+    {
+      name: "slime",
+      level: 2,
+      health: 15
+    },
+    {
+      name: "fanged beast",
+      level: 8,
+      health: 60
+    },
+    {
+      name: "dragon",
+      level: 20,
+      health: 300
+    }
+  ];
 const locations = [
     {
         name: "town square",
@@ -51,6 +68,12 @@ const locations = [
         "button text": ["Fight slime", "Fight fanged beast", "Go to town square"],
         "button functions": [fightSlime, fightBeast, goTown],
         text: "You enter the cave. You see some monsters."
+    },
+    {
+        name: "fight",
+        "button text": ["Attack", "Dodge", "Run"],
+        "button functions": [attack, dodge, goTown],
+        text: "You are fighting a monster."
     }
 ]
 
@@ -80,10 +103,6 @@ function goStore() {
 function goCave() {
     update(locations[2]);
   }
-
-function fightDragon() {
-console.log("Fighting dragon.");
-}
 
 function buyHealth() {
     if (gold >= 10) {
@@ -120,14 +139,41 @@ function sellWeapon() {
     if (inventory.length > 1) {
         gold += 15;
         goldText.innerText = gold;
-        let currentWeapon;
+        let currentWeapon = inventory.shift();
+        text.innerText = "You sold your " + currentWeapon + ".";
+        text.innerText += " In your inventory you have: " + inventory;
+    } else {
+        text.innerText = "Don't sell your only weapon!";
     }
 }
 
 function fightSlime() {
-
+    fighting = 0;
+    goFight();
 }
 
 function fightBeast() {
+    fighting = 1;
+    goFight();
+}
+
+function fightDragon() {
+    fighting = 2;
+    goFight();
+}
+
+function goFight() {
+    update(locations[3]);
+    monsterHealth = monsters[fighting].health;
+    monsterStats.style.display = "block";
+    monsterName.innerText = monsters[fighting].name;
+    monsterHealthText.innerText = monsters[fighting].health;
+}
+
+function attack() {
+
+}
+
+function dodge() {
 
 }
