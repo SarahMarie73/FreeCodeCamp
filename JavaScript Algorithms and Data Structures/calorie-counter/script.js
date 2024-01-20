aea78a0b589ab3f9bcfa76daf35c591a690bb134
Sarah Schlueter <sarahmarie73@gmail.com>
2024-01-20 14:18:54 -0600

    add regex functions for input checks

--- JavaScript Algorithms and Data Structures/calorie-counter/script.js
+++ JavaScript Algorithms and Data Structures/calorie-counter/script.js
@@ -8,6 +8,11 @@ const output = document.getElementById('output');
 let isError = false;
 
 function cleanInputString(str) {
-    const strArray = str.split('');
-    
+    const regex = /[+-\s]/g;
+    return str.replace(regex, '');
+}
+
+function isInvalidInput(str) {
+    const regex = /\d+e\d+/i;
+    return str.match(regext);
 }
