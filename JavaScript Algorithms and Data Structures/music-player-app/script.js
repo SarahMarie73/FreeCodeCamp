96e7dd1e4355f68caa51fea051e57f7809db25f1
Sarah Schlueter <sarahmarie73@gmail.com>
2024-02-19 09:57:21 -0600

    save progress

--- JavaScript Algorithms and Data Structures/music-player-app/script.js
+++ JavaScript Algorithms and Data Structures/music-player-app/script.js
@@ -88,6 +88,8 @@ let userData = {
 
 const renderSongs = (array) => {
     const songsHTML = array.map((song) => {
-        return `<li id="song-${song.id}" class="playlist-song"></li>`;
+        return `<li id="song-${song.id}" class="playlist-song">
+        
+        </li>`;
     })
 };
